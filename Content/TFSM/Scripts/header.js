6bfe862e810dc85d6db0cca81e25f76461427693
Tomás Vera <tvera@artech-consulting.com>
2021-04-09 12:54:43 -0300

    Update header.js

--- Content/TFSM/Scripts/header.js
+++ Content/TFSM/Scripts/header.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
     $("#openMenuBtn").click(() => alert("onclick"));
-    document.getElementById("openMenuBtn").click(() => alert("element click"));
 
-})
-
-function toggleMenu(){
-    alert("click");
-}
+});
 
