3e29558a767b8265687a5320ce1cfa8512746fc6
Tomás Vera <tomas.vera@virtualdreams.io>
2021-04-10 23:28:31 -0300

    update

--- Content/TFSM/Scripts/header.js
+++ Content/TFSM/Scripts/header.js
@@ -1,33 +1,37 @@
-$(document).ready(function(){
-    $(".toggleMenu").click(() => {
-        let overlay = $("#menuOverlay").css("display");
-        document.body.style.overflow = "hidden";
+$(document).ready(function () {
+  $(".toggleMenu").click(() => {
+    let overlay = $("#menuOverlay").css("display");
+    document.body.style.overflow = "hidden";
 
-        $.when(
-            $("#openMenuBtn").toggle(),
-            $("#closeMenuBtn").toggle(),
-            $("#menuOverlay").fadeToggle(100),
-            $("#drawerMenu").toggle("slide", {direction: 'right'})
-        )
-        .then(() => {
-            if(overlay === "block"){
-                document.body.style.overflow = "auto";
-            }
-        })
-
-    });
-
-    $(".deployItems").click(function(){
-        let div = $(this)[0];
-        console.log($(`#${div.id}`).siblings("div[class='deployItems']"));
-        $(`#${div.id}Arrow`).toggleClass("pointRight pointDown");
-        $(`#${div.id}Items`).slideToggle();
-        
-        $(`#${div.id}`).siblings("div[class='deployItems']").each(function(){
-            $(`#${this.id}Arrow`).removeClass("pointDown");
-            $(`#${this.id}Arrow`).addClass("pointRight");
-            $(`#${this.id}Items`).slideUp();
+    $.when(
+      $("#openMenuBtn").toggle(),
+      $("#closeMenuBtn").toggle(),
+      $("#menuOverlay").fadeToggle(100),
+      $("#drawerMenu").toggle("slide", { direction: "right" })
+    ).then(() => {
+      if (overlay === "block") {
+        document.body.style.overflow = "auto";
+        $(".deployItems").each(function () {
+          $(`#${this.id}Arrow`).removeClass("pointDown");
+          $(`#${this.id}Arrow`).addClass("pointRight");
+          $(`#${this.id}Items`).slideUp();
         });
+      }
     });
-});
+  });
 
+  $(".deployItems").click(function () {
+    let div = $(this)[0];
+    console.log($(`#${div.id}`).siblings("div[class='deployItems']"));
+    $(`#${div.id}Arrow`).toggleClass("pointRight pointDown");
+    $(`#${div.id}Items`).slideToggle();
+
+    $(`#${div.id}`)
+      .siblings("div[class='deployItems']")
+      .each(function () {
+        $(`#${this.id}Arrow`).removeClass("pointDown");
+        $(`#${this.id}Arrow`).addClass("pointRight");
+        $(`#${this.id}Items`).slideUp();
+      });
+  });
+});
