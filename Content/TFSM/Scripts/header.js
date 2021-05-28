a34b77c92f49236e53fd15c1cf4c59247eecac73
Tomás Vera <tomas.vera@virtualdreams.io>
2021-05-28 14:38:51 -0300

    Update header.js

--- Content/TFSM/Scripts/header.js
+++ Content/TFSM/Scripts/header.js
@@ -6,12 +6,17 @@ $(document).ready(function () {
     maxlength: jQuery.validator.format("Máximo {0} caracteres"),
     minlength: jQuery.validator.format("Mínimo {0} caracteres"),
   });
-  
-  jQuery.validator.addMethod("isEmail", function(value, element) {
-    let email = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
-    let regex = new RegExp(email);
-    return this.optional(element) || regex.test(value);
-  }, "Ingrese un valor de email válido");
+
+  jQuery.validator.addMethod(
+    "isEmail",
+    function (value, element) {
+      let email =
+        /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+      let regex = new RegExp(email);
+      return this.optional(element) || regex.test(value);
+    },
+    "Ingrese un valor de email válido"
+  );
 
   var deviceWidth = () =>
     window.innerWidth > 0 ? window.innerWidth : screen.width;
@@ -210,36 +215,35 @@ $(document).ready(function () {
     console.log(form);
     $(this).html(form);
   });
-  
-    var nl_validator = $("#newsletter-form").validate({
-      rules: {
-        newsletterEmail: {
-          required: true,
-          email: true,
-        },
-      },
-    });
 
-  $("#newsletter-form").submit(function(e){
+  var nl_validator = $("#newsletter-form").validate({
+    rules: {
+      newsletterEmail: {
+        required: true,
+        email: true,
+      },
+    },
+  });
+
+  $("#newsletter-form").submit(function (e) {
     e.preventDefault();
-    if($(this).valid()){
+    if ($(this).valid()) {
       if (!$("#termsCheckbox").prop("checked")) {
         termsCheckbox = "#termsCheckbox";
         openModal("newsletterTermsModal");
-      }
-      else{
+      } else {
         $("#nl-email").val("");
         $("#termsCheckbox").prop("checked", false);
 
         Toastnotify.create({
           text: "Gracias por registrarte! Serás notificado cuando tengamos cosas nuevas.",
-          duration:5000
+          duration: 5000,
         });
       }
     }
   });
 
-  $("#submit-newsletter").click(function(){
-    $("#newsletter-form").submit();
+  $("#submit-newsletter").click(function () {
+    // $("#newsletter-form").submit();
   });
 });
