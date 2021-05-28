1b0981e0046c339681cb271d80394167fb768f76
Tomás Vera <tomas.vera@virtualdreams.io>
2021-05-28 12:21:20 -0300

    Update header.js

--- Content/TFSM/Scripts/header.js
+++ Content/TFSM/Scripts/header.js
@@ -1,10 +1,16 @@
 jQuery.extend(jQuery.validator.messages, {
-  required: "Este campo es obligatorio.",
+  required: "Este campo es obligatorio",
   email: "Ingrese un valor de email válido.",
-  number: "Ingrese un número válido.",
-  maxlength: jQuery.validator.format("Máximo {0} caracteres."),
-  minlength: jQuery.validator.format("Mínimo {0} caracteres."),
+  number: "Ingrese un número válido",
+  maxlength: jQuery.validator.format("Máximo {0} caracteres"),
+  minlength: jQuery.validator.format("Mínimo {0} caracteres"),
 });
+
+jQuery.validator.addMethod("isEmail", function(value, element) {
+  let email = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+  let regex = new RegExp(email);
+  return this.optional(element) || regex.test(value);
+}, "Ingrese un valor de email válido");
 
 $(document).ready(function () {
   var deviceWidth = () =>
