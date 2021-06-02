887311c4e0541ea6c19f1b109bde6124f4bd4d26
Tomás Vera <tomas.vera@virtualdreams.io>
2021-06-02 13:40:50 -0300

    Update header.js

--- Content/TFSM/Scripts/header.js
+++ Content/TFSM/Scripts/header.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-  // jQuery.extend(jQuery.validator.messages, {
-  //   required: "Este campo es obligatorio",
-  //   email: "Ingrese un valor de email válido",
-  //   number: "Ingrese un número válido",
-  //   maxlength: jQuery.validator.format("Máximo {0} caracteres"),
-  //   minlength: jQuery.validator.format("Mínimo {0} caracteres"),
-  // });
+  jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es obligatorio",
+    email: "Ingrese un valor de email válido",
+    number: "Ingrese un número válido",
+    maxlength: jQuery.validator.format("Máximo {0} caracteres"),
+    minlength: jQuery.validator.format("Mínimo {0} caracteres"),
+  });
 
-  // jQuery.validator.addMethod(
-  //   "isEmail",
-  //   function (value, element) {
-  //     let email =
-  //       /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
-  //     let regex = new RegExp(email);
+  jQuery.validator.addMethod(
+    "isEmail",
+    function (value, element) {
+      let email =
+        /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+      let regex = new RegExp(email);
 
-  //     return regex.test(value);
-  //   },
-  //   "Ingrese un valor de email válido"
-  // );
+      return regex.test(value);
+    },
+    "Ingrese un valor de email válido"
+  );
 
   var deviceWidth = () =>
     window.innerWidth > 0 ? window.innerWidth : screen.width;
@@ -217,14 +217,15 @@ $(document).ready(function () {
     $(this).html(form);
   });
 
-  // var nl_validator = $("#newsletter-form").validate({
-  //   rules: {
-  //     "nl-email": {
-  //       required: true,
-  //       isEmail: true,
-  //     },
-  //   },
-  // });
+  var nl_validator = $("#newsletter-form").validate({
+    rules: {
+      "nl-email": {
+        required: true,
+        isEmail: true,
+
+      },
+    },
+  });
 
   $("#newsletter-form").submit(function (e) {
     e.preventDefault();
