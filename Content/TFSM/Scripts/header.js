3f866aeff5ae5907ffa11301dac22b068940bb3f
Tomás Vera <tomas.vera@virtualdreams.io>
2021-06-10 18:29:49 -0300

    Update header.js

--- Content/TFSM/Scripts/header.js
+++ Content/TFSM/Scripts/header.js
@@ -49,13 +49,13 @@ $(document).ready(function () {
       $("#menuOverlay").fadeToggle(100),
       $("#drawerMenu").toggle("slide", { direction: "right" })
     ).then(() => {
-      if (overlay === "block") {
+      if (overlay === "flex") {
+        document.body.style.overflow = "auto";
         $(".deployItems").each(function () {
           $(`#${this.id}Arrow`).removeClass("pointDown");
           $(`#${this.id}Arrow`).addClass("pointRight");
           $(`#${this.id}Items`).slideUp();
         });
-        document.body.style.overflow = "initial";
       }
     });
   });
@@ -298,24 +298,3 @@ function closeModal(modalId) {
   }
   document.body.style.overflow = "auto";
 }
-
-$(".toggleMenu").click(() => {
-  let overlay = $("#menuOverlay").css("display");
-  document.body.style.overflow = "hidden";
-
-  $.when(
-    $("#openMenuBtn").toggle(),
-    $("#closeMenuBtn").toggle(),
-    $("#menuOverlay").fadeToggle(100),
-    $("#drawerMenu").toggle("slide", { direction: "right" })
-  ).then(() => {
-    if (overlay !== "none") {
-      document.body.style.overflow = "auto";
-      $(".deployItems").each(function () {
-        $(`#${this.id}Arrow`).removeClass("pointDown");
-        $(`#${this.id}Arrow`).addClass("pointRight");
-        $(`#${this.id}Items`).slideUp();
-      });
-    }
-  });
-});
